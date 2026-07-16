f9bc73c65d79efeed7e05a84cef88f2d9636f0eb
minjinseo1221-debug <minjin.seo1221@gmail.com>
2026-07-16 11:53:58 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://zigxvytxpfvtieickvfy.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_ZWUpnl1NEYhG9JnAm_YxtA_ftoPxw-7",
   ADMIN_PASSWORD: "1984"
 };
